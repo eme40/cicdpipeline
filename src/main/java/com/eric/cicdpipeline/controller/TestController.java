3aadf851173cc1a14bf157070200f4e41a199425
eme40 <anayoeric40@gmail.com>
2024-05-22 12:56:44 +0100

    intial commit

---
+++ src/main/java/com/eric/cicdpipeline/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.eric.cicdpipeline.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<Map<String, String>> test(){
+        return ResponseEntity.ok().body(Map.of("Testing", "Up and running"));
+    }
+}
